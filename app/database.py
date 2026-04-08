92e0bcba39dc800c244bf4beb9ca1c21decd4937
shraddha <shraddhashirooru5@gmail.com>
2026-04-08 22:01:07 +0530

    fix db setup

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 from app.config import settings
-DATABASE_URL=settings.database_url
-engine=create_engine(DATABASE_URL)
+DATABASE_URL="sqlite:///./test.db"
+engine=create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.routers import task,user,auth
-
+from app.database import engine
+from app import models
 app=FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 app.include_router(task.router)
 app.include_router(user.router)
