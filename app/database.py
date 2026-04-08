68a8c80e61c28f0a9752209044058281c1102264
shraddha <shraddhashirooru5@gmail.com>
2026-04-08 19:16:18 +0530

    update config and database for postgres

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 from app.config import settings
-DATABASE_URL=f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
-
+DATABASE_URL=settings.database_url
 engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
